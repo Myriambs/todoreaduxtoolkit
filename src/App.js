1a6bf174e07e9e585da72d80af7f0a8256a6fa5d
Myriambs <bensalahmeriam19@gmail.com>
2024-11-19 11:21:47 +0100

    todolist redux

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import TodoAdd from './components/TodoAdd'
+import TodoList from './components/TodoList'
+function App() {
+  return (
+    <div className="App">
+     <TodoAdd/>
+     <TodoList/>
+    </div>
+  );
+}
+
+export default App;
